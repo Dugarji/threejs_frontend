4bcd5e27f562c754cfd4df091f9f1226e16e46ea
Dugarji <dugar6661@gmail.com>
2023-08-24 13:23:21 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import CustomButton from "./CustomButton";
+import AIPicker from '../components/AIPicker'
+import ColorPicker from '../components/ColorPicker'
+import FilePicker from '../components/FilePicker'
+import Tab from '../components/Tab'
+
+export{CustomButton,AIPicker,ColorPicker,FilePicker,Tab};
